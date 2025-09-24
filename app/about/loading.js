b9dc72cff42d00968f6b9751b7dc13cd23968536
EmonHossen10 <emonhossen1000@gmail.com>
2025-09-24 16:19:06 +0600

    loading state added

---
+++ app/about/loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Loading = () => {
+    console.log("hello");
+    return (
+        <div>
+            Loading About page...
+        </div>
+    );
+};
+
+export default Loading;
