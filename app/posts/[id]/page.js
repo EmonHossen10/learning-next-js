d530e1a52fe25955603aeab13520c3f7aedc1660
EmonHossen10 <emonhossen1000@gmail.com>
2025-09-24 19:58:26 +0600

    basic leaarninng finised

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,4 +1,5 @@
 import GetPost from "@/lib/GetPost";
+import GetPostComments from "@/lib/GetPostComment";
 import React from "react";
 
 export async function generateMetadata({ params }) {
@@ -12,12 +13,29 @@ export async function generateMetadata({ params }) {
 
 export default async function PostPage({ params }) {
   const { id } = params;
-  const post = await GetPost(id);
+  const postPromise = GetPost(id);
+  const commentsPromise = GetPostComments(id);
+
+  const [post, comments] = await Promise.all([postPromise, commentsPromise]);
+
   return (
     <div>
       <p className="my-4">Viewing post with ID: {id}</p>
       <h1 className="text-3xl text-green-500 font-bold">{post.title}</h1>
       <p className="mt-4">{post.body}</p>
+      {/* comments added here */}
+
+      
+      <div className="mt-8"></div>
+      <h2 className="text-2xl font-semibold mb-4">Comments</h2>
+      {comments.map((comment) => (
+        <div key={comment.id} className="mb-6 p-4 border rounded-lg">
+          <p className="font-semibold">
+            {comment.name} ({comment.email})
+          </p>
+          <p className="mt-2">{comment.body}</p>
+        </div>
+      ))}
     </div>
   );
 }
